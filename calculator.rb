ea44b908a27f85016f5799173aa9c87398ccdd8e
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-03-07 14:22:35 +1100

    Lewis Coldwell RSpec challenge

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,21 @@
-def add (a, b)
+def add(a, b)
   a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def divide(a, b)
+  a / b
+end
+
+def square(a)
+  a ** 2
 end
 
 # Other methods here :)
---
+++ spec/calculator_test.rb
@@ -0,0 +1,29 @@
+require 'test/unit'
+require_relative '../calculator'
+
+class CalculatorTest < Test::Unit::TestCase
+  def test_add
+    total = add(1, 2)
+    assert_equal(3, total)
+  end
+
+  def test_subtract
+    total = subtract(10, 5)
+    assert_equal(5, total)
+  end
+
+  def test_multiply
+    total = multiply(5, 5)
+    assert_equal(25, total)
+  end
+
+  def test_divide
+    total = divide(10, 2)
+    assert_equal(5, total)
+  end
+
+  def test_square
+    total = square(5)
+    assert_equal(25, total)
+  end
+end
